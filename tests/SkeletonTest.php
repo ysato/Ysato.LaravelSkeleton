1a5ffea1e6adadf16bc2ee8ed3d3b8732ffda4bf
Yoshihiro <56924527+ysato@users.noreply.github.com>
2020-05-01 00:55:54 +0900

    Initial commit

---
+++ tests/SkeletonTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace __Vendor__\__Package__;
+
+use PHPUnit\Framework\TestCase;
+
+class __Package__Test extends TestCase
+{
+    /**
+     * @var __Package__
+     */
+    protected $__PackageVarName__;
+
+    protected function setUp(): void
+    {
+        $this->__PackageVarName__ = new __Package__();
+    }
+
+    public function testIsInstanceOf__Package__(): void
+    {
+        $actual = $this->__PackageVarName__;
+        $this->assertInstanceOf(__Package__::class, $actual);
+    }
+}
